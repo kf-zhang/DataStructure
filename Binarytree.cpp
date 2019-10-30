a5818ae11327c7c1204366ba2dc51c544ec9be71
kf-zhang <46148474+kf-zhang@users.noreply.github.com>
2019-10-30 14:02:29 +0800

    Update Binarytree.cpp
    增加了非递归实现中序 后续遍历，以及非递归实现根据中序先序遍历建树。

--- Binarytree.cpp
+++ Binarytree.cpp
@@ -11,6 +11,12 @@ class BinaryTree
             Node():left(NULL),right(NULL){}
             Node(T x,Node *l=NULL,Node *r=NULL):data(x),left(l),right(r){}
         };
+        struct buildBlock
+        {
+            int pL,pR,mL,mR;
+            buildBlock(int x,int y,int z,int w):pL(x),pR(y),mL(z),mR(w){}
+            buildBlock(){}
+        };
         Node *root;
     public:
         BinaryTree():root(NULL){}
@@ -23,7 +29,41 @@ class BinaryTree
             delete p;
         }
         ~BinaryTree(){ clear(root);}
+        void create(T *mid,T* pre,int n)  //非递归实现根据中序先序遍历建树,rootb必须为空，否则会丢失之前的数据
+        {
+            if(n<=0)
+                return;
+            linkStack<buildBlock> bS;
+            linkStack<Node *> nS;
+            buildBlock b(0,n-1,0,n-1);
+            buildBlock tmp;
+            Node *p;
+            root=new Node(pre[0]);
+            int i;
+            int interval;
+            nS.push(root);
+            bS.push(b);
+            while(!nS.isEmpty())
+            {
+                p=nS.pop();
+                b=bS.pop();
 
+                for(i=b.mL;i<=b.mR&&mid[i]!=pre[b.pL];++i);
+                interval=i-1-b.mL;
+                if(b.pL+2+interval<=b.pR){ //判断条件
+                    p->right=new Node(pre[b.pL+2+interval]);
+                    nS.push(p->right);
+                    tmp=buildBlock(b.pL+2+interval,b.pR,i+1,b.mR);
+                    bS.push(tmp);
+                }
+                if(0<=interval){  //判断条件
+                    p->left=new Node(pre[b.pL+1]);
+                    nS.push(p->left);
+                    tmp=buildBlock(b.pL+1,b.pL+1+interval,b.mL,b.mL+interval);
+                    bS.push(tmp);
+                }
+            }
+        }
         void create(T flag,T quit)
         {
             T data;
@@ -98,10 +138,6 @@ class BinaryTree
             preOrderR(p->left);
             preOrderR(p->right);
         }
-        // void buildTreeBasedOnPreMid(T *pre,T *Mid)
-        // {
-
-        // }
         void levelOrder()
         {
             if(!root) return;
@@ -114,6 +150,73 @@ class BinaryTree
                 std::cout<<p->data<<' ';
                 if(p->left) q.enQueue(p->left);
                 if(p->right)    q.enQueue(p->right);
+            }
+        }
+        void midOrder()
+        {
+            if(!root) return;
+            linkStack<Node *> s;
+            Node *p=root;
+            s.push(root);
+            while(p->left)
+            {
+                s.push(p->left);
+                p=p->left;
+            }
+            while(!s.isEmpty())
+            {
+                p=s.Top();
+                s.pop();
+                std::cout<<p->data<<' ';
+                if(p->right)
+                {
+                    p=p->right;
+                    s.push(p);
+                    while(p->left)
+                    {
+                        s.push(p->left);
+                        p=p->left;
+                    }
+                }
+            }
+
+        }
+        void postOrder()
+        {
+            if(!root) return;
+            linkStack<Node*> nS;
+            linkStack<int> tS;//维护node 访问次数的栈
+            Node *p;
+            int zero=0;
+            int time;
+            nS.push(root);
+            tS.push(zero);
+            while(!nS.isEmpty())
+            {
+                p=nS.pop();
+                time=tS.pop();
+                time++;       //time记录的是访问该节点的次数
+                switch(time)
+                {
+                    case 3: std::cout<<p->data<<' ';
+                            break;
+                    case 2: 
+                            nS.push(p);
+                            tS.push(time);
+                            if(p->right){       
+                                nS.push(p->right);
+                                tS.push(zero);
+                            }
+                            break;
+                    case 1:
+                            nS.push(p);
+                            tS.push(time);
+                            if(p->left){
+                                nS.push(p->left);
+                                tS.push(zero);
+                            }
+                            break;
+                }
             }
         }
         void preOrder()    //非递归实现
@@ -136,8 +239,13 @@ class BinaryTree
 int main()
 {
     BinaryTree<int> tree;
-    tree.create(-1,-2);
-    tree.levelOrder();
+    //tree.create(-1,-2);
+    int pre[]={0,1,3,5,6,8,7,2,4};
+    int mid[]={8,6,5,7,3,1,0,2,4};
+    tree.create(mid,pre,9);
+    tree.preOrder();
+    tree.midOrder();
+    std::cout<<std::endl;
     std::cout<<tree.height();
     return 0;
 }
